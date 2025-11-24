7af2ac2b43a12ca9cba6f71681ecd2fd01a825c9
Sayeed <sayeedssaadman272491@gmail.com>
2025-11-24 12:30:51 +0600

    User data fetching to admin

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using AgroManagement.Data;
+using AgroManagement.Models;
+using System.Linq;
 
 public class AdminController : Controller
 {
@@ -10,6 +12,7 @@ public class AdminController : Controller
         _context = context;
     }
 
+    // Dashboard
     public IActionResult Dashboard()
     {
         var totalAnimals = _context.Animals.Count();
@@ -20,4 +23,96 @@ public class AdminController : Controller
 
         return View();
     }
+
+    // -----------------------------
+    // MANAGE USERS (LIST)
+    // -----------------------------
+    public IActionResult ManageUsers()
+    {
+        var users = _context.Users.ToList();
+        return View(users);
+    }
+
+    // -----------------------------
+    // USER DETAILS
+    // -----------------------------
+    public IActionResult UserDetails(int id)
+    {
+        var user = _context.Users.FirstOrDefault(u => u.Id == id);
+        if (user == null) return NotFound();
+
+        return View(user);
+    }
+
+    // -----------------------------
+    // EDIT USER (GET)
+    // -----------------------------
+    public IActionResult EditUser(int id)
+    {
+        var user = _context.Users.Find(id);
+        if (user == null) return NotFound();
+
+        return View(user);
+    }
+
+    // -----------------------------
+    // EDIT USER (POST)
+    // -----------------------------
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public IActionResult EditUser(User user)
+    {
+        if (!ModelState.IsValid)
+        {
+            return View(user);
+        }
+
+        _context.Users.Update(user);
+        _context.SaveChanges();
+        return RedirectToAction("ManageUsers");
+    }
+
+    // -----------------------------
+    // DELETE USER (GET)
+    // -----------------------------
+    public IActionResult DeleteUser(int id)
+    {
+        var user = _context.Users.Find(id);
+        if (user == null) return NotFound();
+
+        return View(user);
+    }
+
+    // -----------------------------
+    // DELETE USER (POST)
+    // -----------------------------
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public IActionResult DeleteUserConfirmed(int id)
+    {
+        var user = _context.Users.Find(id);
+        if (user != null)
+        {
+            _context.Users.Remove(user);
+            _context.SaveChanges();
+        }
+
+        return Json(new { ok = true });
+
+    }
+
+    // -----------------------------
+    // FETCH PASSWORD FROM DATABASE
+    // -----------------------------
+    [HttpGet]
+    public IActionResult GetUserPassword(int id)
+    {
+        var user = _context.Users.FirstOrDefault(u => u.Id == id);
+        if (user == null)
+            return Json(new { success = false, password = "" });
+
+        // returning PasswordHash (because that's what you store)
+        return Json(new { success = true, password = user.PasswordHash });
+    }
+
 }
